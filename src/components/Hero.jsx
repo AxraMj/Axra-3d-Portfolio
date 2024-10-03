9697f39feb83de9ef6a57581b7aeacf60ffe609c
AxraMj <offxaxra@gmail.com>
2024-10-03 22:55:35 +0530

    errorupdate changed 2 commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,13 @@
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -18,14 +19,13 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Axra</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a Software Developer, who also <br className='sm:block hidden' />
-            works as a Copywriter.
+          I am a Software Developer ,  who also <br className='sm:block hidden' />
+          works as a Copywriter.
           </p>
         </div>
       </div>
 
-      {/* Remove ComputersCanvas to eliminate the 3D renderer */}
-      {/* <ComputersCanvas /> */}
+      <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
